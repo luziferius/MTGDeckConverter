797abe4e4af46f6ebf71d979252a5801d8fac2c0
Thomas Hess <thomas.hess@udo.edu>
2019-12-22 15:10:16 +0100

    Database: Implemented importing Scryfall data using the bulk data API.
    - Use the requests library to read the JSON card list from the Scryfall API.
    - Optionally use a file system path as a replacement
      to fill the database with a local, existing dump.
    - INSERT all received cards into the database.
    - Increased the minimum compatible database schema version.

--- MTGDeckConverter/card_db/db.py
+++ MTGDeckConverter/card_db/db.py
@@ -14,10 +14,13 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import atexit
+from http import HTTPStatus
 import importlib.resources
 import sqlite3
-from typing import NamedTuple, Union
+from typing import NamedTuple, Union, List
 from pathlib import Path
+
+import requests
 
 from MTGDeckConverter.logger import get_logger
 
@@ -43,7 +46,7 @@ class CardDatabase:
     #       Either implement an update function or simply drop the content
     #       and re-download everything.
 
-    COMPATIBLE_SCHEMA_VERSIONS = CompatibleSchemaVersions(0, 2)
+    COMPATIBLE_SCHEMA_VERSIONS = CompatibleSchemaVersions(2, 3)
 
     def __init__(self, database_path: Union[str, Path], do_validate_schema: bool = True):
         logger.info(f"About to open database: {database_path}, validating schema: {do_validate_schema}")
@@ -98,3 +101,74 @@ class CardDatabase:
             self.db.rollback()
             self.db.close()
             self.db = None
+
+    def is_database_populated(self) -> bool:
+        result = self.db.execute(
+            "SELECT EXISTS( "
+            "SELECT * "
+            "FROM Printing)").fetchone()[0]
+        return bool(result)
+
+    def populate_database(self, path_to_data: Path = None):
+        if self.is_database_populated():
+            logger.warning("The database already contains data. Skipping the population process.")
+            return
+        card_data_list = _request_scryfall_card_data(path_to_data)
+        cursor = self.db.cursor()
+        self.db.rollback()
+        cursor.execute("BEGIN TRANSACTION")
+        try:
+            for card in card_data_list:
+                oracle_id = card["oracle_id"]
+                set_abbr = card["set"]
+                if not bool(cursor.execute(
+                        "SELECT EXISTS( "
+                        "SELECT * "
+                        "FROM Card "
+                        "WHERE Scryfall_Oracle_ID = ?)", (oracle_id,)).fetchone()[0]):
+                    cursor.execute("INSERT INTO Card (English_Name, Scryfall_Oracle_ID) "
+                                   "VALUES (?, ?)", (card["name"], oracle_id))
+                if not bool(cursor.execute(
+                        "SELECT EXISTS( "
+                        "SELECT *"
+                        "FROM Card_Set "
+                        "WHERE Abbreviation = ?)", (set_abbr,)).fetchone()[0]):
+                    cursor.execute("INSERT INTO Card_Set (English_Name, Abbreviation) "
+                                   "VALUES (?, ?)", (card["set_name"], set_abbr))
+                # Do the table ID lookups by the unique set abbreviation and oracle id
+                cursor.execute("INSERT INTO Printing (Card_ID, Set_ID, Collector_Number, Scryfall_Card_ID) "
+                               "SELECT Card_ID, Set_ID, ?, ? "
+                               "FROM Card_Set INNER JOIN Card "  # This joins two unconnected relations
+                               "WHERE Card_Set.Abbreviation = ? "  # and filters for the single tuple that contains
+                               "AND Card.Scryfall_Oracle_ID = ?",  # both required unique IDs
+                               (card["collector_number"], card["id"], set_abbr, oracle_id))
+        except Exception as e:
+            self.db.rollback()
+            raise e
+        else:
+            self.db.commit()
+
+
+def _request_scryfall_card_data(path_to_data: Path = None) -> List[dict]:
+    """
+    Use the Scryfall API bulk data end point to download the card data.
+    See the API documentation: https://scryfall.com/docs/api/bulk-data
+    The returned list contains > 50000 card entries (as of December 2019).
+
+    If path_to_data is given, the file content will be used as a substitute.
+    This is factored out into a static function used by the CardDatabase class to aid testing.
+    Mock this function for unit tests that should not access the online API.
+
+    """
+    if path_to_data is None:
+        card_data_request = requests.get("https://archive.scryfall.com/json/scryfall-default-cards.json")
+        if card_data_request.status_code != HTTPStatus.OK:
+            error_msg = f"Request to download the card data failed with status code {card_data_request.status_code}"
+            logger.error(error_msg)
+            raise RuntimeError(error_msg)
+        card_data_list: List[dict] = card_data_request.json()
+        logger.info("Requested the card data from the Scryfall bulk data API end point.")
+    else:
+        import json
+        card_data_list = json.loads(path_to_data.read_text())
+    return card_data_list
